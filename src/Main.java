68a55575baa1fa293de2ecb99ac8b80f8ca133a6
mrerro <Dyakov.hellvisionstudio@gmail.com>
2015-07-07 23:32:41 +0500

    clear main

--- src/Main.java
+++ src/Main.java
@@ -15,14 +15,6 @@ public class Main {
     public static void main(String[] args) throws IOException, ParseException {
         String mainUrl = config.getDownloadURIString();
         String[] files = config.getDownloadFiles();
-
-<<<<<<< HEAD
-        String config = "Hello World";
-        System.out.println(config);
-        Updater.checkUpdates(mainUrl, files);
-        System.out.println("Main off");
-=======
         Test test = new Test(config);
->>>>>>> origin/master
     }
 }
