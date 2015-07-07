4132c7811adb25453302b5e82ffedc9e62001517
MrErrorman <errorman.gamer@gmail.com>
2015-07-07 22:51:44 +0500

    Add a test Class /src/Test.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
-import tools.*;
-import auth.*;
+import auth.Account;
+import config.Config;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * Created by John on 06.07.2015.
  */
 public class Main {
+    public static config.Config config = new Config();
 
+    
     public static void main(String[] args) throws IOException, ParseException {
-        String mainUrl = "http://files.dmine.esy.es/";
-        String[] files = {"versions.json", "config.zip", "core.zip", "libraries.zip", "mods.zip", "tlauncher.zip"};
+        String mainUrl = config.getDownloadURIString();
+        String[] files = config.getDownloadFiles();
 
-        String config = "Hello World";
-        System.out.println(config);
+        Test test = new Test(config);
     }
 }
